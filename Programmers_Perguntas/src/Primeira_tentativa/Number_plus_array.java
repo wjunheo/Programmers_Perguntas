c46b5d4527a9d0533d17bfa4ed4366e6e87ee2ac
:wjunheo <wonjunheors@gmail.com>
2021-09-06 23:56:58 +0900

    6/set

---
+++ Programmers_Perguntas/src/Primeira_tentativa/Number_plus_array.java
@@ -0,0 +1,18 @@
+package Primeira_tentativa;
+
+import java.util.Scanner;
+
+public class Number_plus_array {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int x= sc.nextInt();
+		int n= sc.nextInt();
+		long[] answer= new long[n];
+		
+		for(int i=0; i<n; i++) {
+			answer[i]=x*(1+i);
+			
+		}
+		System.out.println(answer);
+	}
+}
