03d9a1f277bbc7457b5ac7f440f779f2504ed831
:wjunheo <wonjunrs@gmail.com>
2021-11-02 20:15:31 +0900

    2/Nov

---
+++ Programmers_Perguntas/src/Primeira_tentativa/withoutnumber.java
@@ -0,0 +1,16 @@
+package Primeira_tentativa;
+
+public class withoutnumber {
+	public static void main(String[] args) {
+		 int [] numbers = {1,2,3,10};
+				int answer = 45;
+				for(int sem : numbers) {
+					answer-=sem;
+				}
+				System.out.println(answer);
+	}
+	
+
+			
+		}
+
