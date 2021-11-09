744edbb0e4e3333f5d57c4dd7f00201c7e27cdae
wjunheo <wonjunrs@gmail.com>
2021-11-09 23:57:44 +0900

    9/Nov

---
+++ Programmers_Perguntas/src/segunda_tentativa/numbersumH.java
@@ -0,0 +1,18 @@
+package segunda_tentativa;
+
+public class numbersumH {
+	boolean solution(int x) {
+	boolean answer=true;
+	int num=x;
+	int sum =0;
+	while(num!=0) {
+		sum+= num%10;
+		num=num/10;
+	}
+	if(x%sum !=0) { 
+		answer =false;
+	}
+	return answer;
+
+}
+}
