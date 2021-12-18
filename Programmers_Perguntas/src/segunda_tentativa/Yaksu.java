d84680bb39c9da46cc0ce21c574d4c57135bfc77
wjunheo <wonjunrs@gmail.com>
2021-12-18 21:56:51 +0900

    18/restart

---
+++ Programmers_Perguntas/src/segunda_tentativa/Yaksu.java
@@ -0,0 +1,21 @@
+package segunda_tentativa;
+
+import java.util.Scanner;
+
+public class Yaksu {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N= sc.nextInt();
+		int sum =0;
+		for ( int i=1; i<=N;i++) {
+			if(N%i==0) {
+				System.out.println("¾à¼ö"+i);
+				sum+=i;
+				
+			}
+		}System.out.println(sum);
+		
+	}
+
+}
