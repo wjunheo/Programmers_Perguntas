db57b360706c0929572a7c209acd3e7e802df82c
wjunheo <wonjunrs@gmail.com>
2021-12-22 23:55:50 +0900

    22/dez

--- Programmers_Perguntas/src/segunda_tentativa/Number_English_1.java
+++ Programmers_Perguntas/src/segunda_tentativa/Number_English_1.java
@@ -5,7 +5,8 @@ public class Number_English_1 {
 }
 class Solution {
     public int solution(String s) {
-    	String [] arr= {"zero","one","two","three","four","five","six","seven","eight","nine","ten"};
+    	String [] arr= {"zero","one","two","three","four",
+    			"five","six","seven","eight","nine","ten"};
 		for(int i =0; i<arr.length; i++) {
 			s= s.replace(arr[i], String.valueOf(i));
 		}
